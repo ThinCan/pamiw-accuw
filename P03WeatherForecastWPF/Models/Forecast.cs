795ae97e7724af5250c0e41f63220e13bcb464cf
ThinCan <01169581@pw.edu.pl>
2023-10-10 16:50:58 +0200

    added 6 endpoints

---
+++ P03WeatherForecastWPF/Models/Forecast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03WeatherForecastWPF.Client.Models
+{
+    public class ForecastTemperature
+    {
+        public string Unit;
+        public double Value;
+    }
+    internal class Forecast
+    {
+        public long EpochTime { get; set; }
+        public string DateTime { get; set; }
+        public string IconPhrase { get; set; }
+        public ForecastTemperature Temperature { get; set; }
+    }
+}
---
+++ P03WeatherForecastWPF/Models/WeatherAlarm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03WeatherForecastWPF.Client.Models
+{
+    public class Alarm
+    {
+        public string AlarmType;
+    }
+
+    internal class WeatherAlarm
+    {
+        public string Date { get; set; }
+        public Alarm Alarms;
+
+    }
+}
